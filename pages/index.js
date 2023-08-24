b8d648e3bf54ff344d4954a49dff05763f6c0a8f
kimcipher <kimutaij192@gmail.com>
2023-08-24 14:37:17 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Navbar />
       <Head>
         <title>Create Next App</title>
       </Head>
